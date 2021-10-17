e52ff603982ea5009a82298f4539227b139a57c9
Gabor Papp <gabor.papp@gmail.com>
2021-10-17 22:33:09 +0200

    CMake fix for Cinder-OpenCV4

--- proj/cmake/cinder-blobtracker-config.cmake
+++ proj/cmake/cinder-blobtracker-config.cmake
@@ -2,10 +2,11 @@ if ( NOT TARGET Cinder-BlobTracker )
 	get_filename_component( CINDER_BLOBTRACKER_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
 
 	get_filename_component( CINDER_OPENCV4_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../Cinder-OpenCV4" ABSOLUTE )
-	find_package( Cinder-OpenCV4 PATHS CINDER_OPENCV4_PATH NO_DEFAULT_PATH )
+	find_package( Cinder-OpenCV4 PATHS "${CINDER_OPENCV4_PATH}/proj/cmake" NO_DEFAULT_PATH )
 
 	set( CINDER_BLOBTRACKER_INCLUDES
 		${CINDER_BLOBTRACKER_PATH}/include
+		${Cinder-OpenCV4_INCLUDES}
 	)
 	set( CINDER_BLOBTRACKER_SOURCES
 		${CINDER_BLOBTRACKER_PATH}/src/mndl/blobtracker/BlobTracker.cpp
@@ -15,7 +16,7 @@ if ( NOT TARGET Cinder-BlobTracker )
 	add_library( Cinder-BlobTracker ${CINDER_BLOBTRACKER_SOURCES} )
 
 	target_include_directories( Cinder-BlobTracker PUBLIC "${CINDER_BLOBTRACKER_INCLUDES}"
-		PRIVATE "${CINDER_OPENCV4_PATH}/include" ${Cinder-OpenCV4_INCLUDES} )
+		PRIVATE ${Cinder-OpenCV4_INCLUDES} )
 
 	target_include_directories( Cinder-BlobTracker SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
 
